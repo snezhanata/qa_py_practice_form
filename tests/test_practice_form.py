7dee5ae321898aa9746663943347eeecdf79b86b
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-09-12 11:56:20 +0400

    calendar checks were updated

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -16,9 +16,9 @@ def test_submit_student_registration_form():
     browser.element('#gender-radio-2').double_click()
     browser.element('#userNumber').type('0123401234')
     browser.element('#dateOfBirthInput').click()
-    browser.element('[value="7"]').click()  # Calendar: Month
-    browser.element('[value="2000"]').click()  # Calendar: Year
-    browser.element('[aria-label="Choose Tuesday, August 8th, 2000"]').click()  # Calendar: Day
+    browser.element('.react-datepicker__year-select').element('[value="2000"]').click()
+    browser.element('.react-datepicker__month-select').element('[value="7"]').click()
+    browser.element('.react-datepicker__day--008').click()
     # we can set value in Calendar also just using: browser.element('#dateOfBirthInput').type('01 Sep 2001')
     browser.element('#subjectsInput').type('Maths').press_enter()
     browser.element('[for="hobbies-checkbox-1"]').click()
@@ -30,6 +30,16 @@ def test_submit_student_registration_form():
     browser.element('#react-select-4-input').type('Agra').press_enter()
     browser.element('#submit').press_enter()
 
-    browser.all('.table-responsive').should(have.texts(
-        'Label Values\nStudent Name Nyan Cat\nStudent Email nyan.cat@mail.com\nGender Female\nMobile 0123401234\nDate of Birth 08 August,2000\nSubjects Maths\nHobbies Sports\nPicture pic.jpg\nAddress https://www.youtube.com/watch?v=QH2-TGUlwu4\nState and City Uttar Pradesh Agra'
-    ))
+    # browser.all('.table-responsive').should(have.texts(
+    #     # 'Label Values\nStudent Name Nyan Cat\nStudent Email nyan.cat@mail.com\nGender Female\nMobile 0123401234\nDate of Birth 08 August,2000\nSubjects Maths\nHobbies Sports\nPicture pic.jpg\nAddress https://www.youtube.com/watch?v=QH2-TGUlwu4\nState and City Uttar Pradesh Agra'
+    # ))
+    browser.all('table tr')[1].all('td')[1].should(have.exact_text('Nyan Cat'))
+    browser.all('table tr')[2].all('td')[1].should(have.exact_text('nyan.cat@mail.com'))
+    browser.all('table tr')[3].all('td')[1].should(have.exact_text('Female'))
+    browser.all('table tr')[4].all('td')[1].should(have.exact_text('0123401234'))
+    browser.all('table tr')[5].all('td')[1].should(have.exact_text('08 August,2000'))
+    browser.all('table tr')[6].all('td')[1].should(have.exact_text('Maths'))
+    browser.all('table tr')[7].all('td')[1].should(have.exact_text('Sports'))
+    browser.all('table tr')[8].all('td')[1].should(have.exact_text('pic.jpg'))
+    browser.all('table tr')[9].all('td')[1].should(have.exact_text('https://www.youtube.com/watch?v=QH2-TGUlwu4'))
+    browser.all('table tr')[10].all('td')[1].should(have.exact_text('Uttar Pradesh Agra'))
