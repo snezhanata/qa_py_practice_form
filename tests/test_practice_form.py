ec32ff3efc89543922a07d4061524c58fafddced
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-09-14 00:52:27 +0400

    calendar checks were updated

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -30,12 +30,24 @@ def test_submit_student_registration_form():
     browser.element('#react-select-4-input').type('Agra').press_enter()
     browser.element('#submit').press_enter()
 
-    # way1
+    # Checking Results
+    browser.all('table tr')[1].all('td')[1].should(have.exact_text('Nyan Cat'))
+    browser.all('table tr')[2].all('td')[1].should(have.exact_text('nyan.cat@mail.com'))
+    browser.all('table tr')[3].all('td')[1].should(have.exact_text('Female'))
+    browser.all('table tr')[4].all('td')[1].should(have.exact_text('0123401234'))
+    browser.all('table tr')[5].all('td')[1].should(have.exact_text('08 August,2000'))
+    browser.all('table tr')[6].all('td')[1].should(have.exact_text('Maths'))
+    browser.all('table tr')[7].all('td')[1].should(have.exact_text('Sports'))
+    browser.all('table tr')[8].all('td')[1].should(have.exact_text('pic.jpg'))
+    browser.all('table tr')[9].all('td')[1].should(have.exact_text('https://www.youtube.com/watch?v=QH2-TGUlwu4'))
+    browser.all('table tr')[10].all('td')[1].should(have.exact_text('Uttar Pradesh Agra'))
+
+    # way_2
     # browser.all('.table-responsive').should(have.exact_texts(
     #     'Label Values\nStudent Name Nyan Cat\nStudent Email nyan.cat@mail.com\nGender Female\nMobile 0123401234\nDate of Birth 08 August,2000\nSubjects Maths\nHobbies Sports\nPicture pic.jpg\nAddress https://www.youtube.com/watch?v=QH2-TGUlwu4\nState and City Uttar Pradesh Agra'
     # ))
 
-    # way2
+    # way_3
     # browser.all(".modal-body td:nth-child(even)").should(have.texts(
     #     # 'Label Values\nStudent Name Nyan Cat\nStudent Email nyan.cat@mail.com\nGender Female\nMobile 0123401234\nDate of Birth 08 August,2000\nSubjects Maths\nHobbies Sports\nPicture pic.jpg\nAddress https://www.youtube.com/watch?v=QH2-TGUlwu4\nState and City Uttar Pradesh Agra'
     #     'Nyan Cat',
@@ -49,15 +61,3 @@ def test_submit_student_registration_form():
     #     'https://www.youtube.com/watch?v=QH2-TGUlwu4',
     #     'Uttar Pradesh Agra'
     # ))
-
-    # way3
-    browser.all('table tr')[1].all('td')[1].should(have.exact_text('Nyan Cat'))
-    browser.all('table tr')[2].all('td')[1].should(have.exact_text('nyan.cat@mail.com'))
-    browser.all('table tr')[3].all('td')[1].should(have.exact_text('Female'))
-    browser.all('table tr')[4].all('td')[1].should(have.exact_text('0123401234'))
-    browser.all('table tr')[5].all('td')[1].should(have.exact_text('08 August,2000'))
-    browser.all('table tr')[6].all('td')[1].should(have.exact_text('Maths'))
-    browser.all('table tr')[7].all('td')[1].should(have.exact_text('Sports'))
-    browser.all('table tr')[8].all('td')[1].should(have.exact_text('pic.jpg'))
-    browser.all('table tr')[9].all('td')[1].should(have.exact_text('https://www.youtube.com/watch?v=QH2-TGUlwu4'))
-    browser.all('table tr')[10].all('td')[1].should(have.exact_text('Uttar Pradesh Agra'))
